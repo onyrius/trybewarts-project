b1a2348180fd0cd9032ffeed74f92a0acd13f141
Suelen <suelenonyrius@gmail.com>
2021-12-04 01:35:36 -0300

    requisito 20 e outras alteraçoes

--- script.js
+++ script.js
@@ -30,6 +30,7 @@ https://www.horadecodar.com.br/2020/07/28/como-verificar-se-um-checkbox-esta-che
 https://www.w3schools.com/jsref/prop_checkbox_checked.asp
 https://qastack.com.br/programming/3014649/how-to-disable-html-button-using-javascript */
 
+// Cria display demonstrativo do decremento de caracteres
 const displayCaracteresDecremento = document.querySelector('#counter');
 const textArea = document.getElementsByTagName('textarea')[0];
 
@@ -37,3 +38,22 @@ textArea.addEventListener('keyup', (event) => {
   const decremento = event.target.maxLength - event.target.textLength;
   displayCaracteresDecremento.innerHTML = `máximo ${decremento}`;
 });
+
+/** Source https://www.youtube.com/watch?v=LiQcxaC2Ek4&ab_channel=WebdesignemFoco */
+
+//
+/* Todos os campos do formulário devem ser substituídos.
+Deve haver um campo com o formato "Nome: Alguem Aqui"
+Deve haver um campo com o formato "Email: email@mail.com"
+Deve haver um campo com o formato "Casa: Casa Escolhida"
+Deve haver um campo com o formato "Família: Família Escolhida"
+Deve haver um campo com o formato "Matérias: Matérias, Marcadas, Aqui"
+Deve haver um campo com o formato "Avaliação: NotaAqui"
+Deve haver um campo com o formato "Observações: Observações aqui" */
+
+// const formulario = document.getElementsByTagName('form')[0];
+// console.log(formulario);
+// formulario.addEventListener('click',escolher);
+
+// function escolher () {
+// }
